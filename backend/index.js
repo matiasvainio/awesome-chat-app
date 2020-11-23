c72c2fa4a39d435ff9e14a1acfb6de4cf21c4f8d
matiasva <matias.vainio@metropolia.fi>
2020-11-23 13:45:21 +0200

    linter setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
 const port = 3000;
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
+app.get('/', (request, response) => {
+  response.send('Hello World!');
 });
 
 app.listen(port, () => {
