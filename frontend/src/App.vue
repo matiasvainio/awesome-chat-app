48e183c21e0016c2f0e5477b59621663db0628a5
matiasva <matias.vainio@metropolia.fi>
2020-11-23 15:26:04 +0200

    feature: show messages on display

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <h1>Awesome chat-app</h1>
+    <ChatMessages :messages="messages" />
+    <MessageForm @add-message="addMessage" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import ChatMessages from '@/components/ChatMessages.vue';
+import MessageForm from '@/components/MessageForm.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    ChatMessages,
+    MessageForm,
+  },
+  data() {
+    return {
+      messages: [
+        {
+          id: 1,
+          content: 'Lorem ipsum dolor sit amet.',
+        },
+        {
+          id: 2,
+          content: 'Suspendisse vehicula lacus ut tellus.',
+        },
+        {
+          id: 3,
+          content: 'Vivamus malesuada egestas urna, ac.',
+        },
+      ],
+    };
+  },
+  methods: {
+    addMessage(message) {
+      this.messages = [...this.messages, message];
+    },
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
