158f4e2f3b48c8197f6ac6113bc24bd58e59d80a
Joni Laukkanen <joni.laukkanen@metropolia.fi>
2020-11-24 17:30:40 +0200

    gcloud and mongodb atlas connection working

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 const path = require(`path`);
 const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://sudo:sudo123@chatapp.7etcu.mongodb.net/chatapp?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
-app.use(bodyParser.urlencoded({ extended: true }));
+client.connect(err => {})
+
+app.get('/', (req, res) => {
+  res.send('Hello from App Engine!');
+});
 
 app.get('/submit', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/form.html'));
-  });
+  res.sendFile(path.join(__dirname, '/views/form.html'));
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
@@ -16,9 +25,14 @@ app.listen(PORT, () => {
 });
 
 app.post('/submit', (req, res) => {
-    console.log({
-      name: req.body.name,
-      message: req.body.message
-    });
-    res.send('Thanks for your message!');
+  console.log({
+    name: req.body.name,
+    message: req.body.message
+  });
+    const collection = client.db("chatapp").collection("message");
+    let  message = {sender: req.body.name, content: req.body.message}; 
+    // perform actions on the collection object
+    collection.insertOne(message);
+    console.log("1 document inserted");
+    res.sendFile(path.join(__dirname, '/views/form.html'));
   });
