44c97351bb8a9c370de1c09eee78be69a6cbaf3e
matiasva <matias.vainio@metropolia.fi>
2020-11-27 12:12:36 +0200

    feature: mongoose implementation

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,57 @@
 const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-
-const uri = "mongodb+srv://sudo:sudo123@chatapp.7etcu.mongodb.net/chatapp?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
+const router = express.Router();
+
+app.use(cors());
+
+const uri =
+  'mongodb+srv://sudo:sudo123@chatapp.7etcu.mongodb.net/chatapp?retryWrites=true&w=majorityy';
+// const client = new MongoClient(uri, {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+// });
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('mongodb connection established');
+});
+
 const port = 3000;
 
-app.get((req, res) => {
-  response.send('Hello World!');
-  });
+const schema = new mongoose.Schema({
+  content: String,
+});
+
+schema.set('toJSON', {
+  transform: (doc, object) => {
+    const returnableObject = object;
+    // eslint-disable-next-line no-underscore-dangle
+    returnableObject.id = object._id.toString();
+    // eslint-disable-next-line no-underscore-dangle
+    delete returnableObject._id;
+    // eslint-disable-next-line no-underscore-dangle
+    delete returnableObject.__v;
+    return returnableObject;
+  },
+});
+
+const Message = mongoose.model('Message', schema);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/messages', async (req, res) => {
+  const foo = await Message.find({});
+  res.json(foo.map((o) => o.toJSON()));
+});
 
 app.listen(port, () => {
   console.log(`app listenting at http://localhost:${port}`);
-});
-
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
 });
--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,16 @@ const express = require('express');
 const app = express();
 const path = require(`path`);
 const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt')
-const {MongoClient} = require('mongodb');
+const bcrypt = require('bcrypt');
+const { MongoClient } = require('mongodb');
 
-const uri = "mongodb+srv://sudo:sudo123@chatapp.7etcu.mongodb.net/chatapp?retryWrites=true&w=majority";
+const uri =
+  'mongodb+srv://sudo:sudo123@chatapp.7etcu.mongodb.net/chatapp?retryWrites=true&w=majority';
 const client = new MongoClient(uri, { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-client.connect(err => {})
+client.connect((err) => {});
 
 app.get('/', (req, res) => {
   res.send('Hello from App Engine!');
@@ -38,61 +39,61 @@ app.listen(PORT, () => {
 
 //Creates new message with sender and content to message collection
 app.post('/submit', (req, res) => {
-
   console.log({
     name: req.body.name,
-    message: req.body.message
+    message: req.body.message,
   });
 
-  const collection = client.db("chatapp").collection("message");
-  const  message = {sender: req.body.name, content: req.body.message};
+  const collection = client.db('chatapp').collection('message');
+  const message = { sender: req.body.name, content: req.body.message };
 
   // perform actions on the collection object
   collection.insertOne(message);
-  console.log("1 document inserted");
+  console.log('1 document inserted');
   res.redirect('/submit');
 
-  const messages = collection.find().toArray()
-  .then(results => {
-    console.log(results)
-  })
-  .catch(error => console.error(error))
+  const messages = collection
+    .find()
+    .toArray()
+    .then((results) => {
+      console.log(results);
+    })
+    .catch((error) => console.error(error));
 });
 
 //Checks username
 app.post('/login', async (req, res) => {
-
   const currentUser = req.body.username;
   const password = req.body.password;
 
-  const collection = client.db("chatapp").collection("user");
+  const collection = client.db('chatapp').collection('user');
 
   async function checkUser(username, password) {
     //... fetch user from a db etc.
 
-    const query = {username: username};
-    const options = {projection: { _id: 0, password: 1}};
+    const query = { username: username };
+    const options = { projection: { _id: 0, password: 1 } };
 
-    const user = await collection.findOne(query, options );
+    const user = await collection.findOne(query, options);
 
     console.log(user);
- 
+
     const match = await bcrypt.compare(password, user.password);
- 
-    if(match) {
-      console.log("LOGIN");
+
+    if (match) {
+      console.log('LOGIN');
     } else {
-      console.log("ERROR");
+      console.log('ERROR');
     }
-}
+  }
   checkUser(currentUser, password);
   res.redirect('/login');
 });
 
 //Creates new document with username, password and hashed password to user collection
 app.post('/signup', async (req, res) => {
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(req.body.password, saltRounds)
+  const saltRounds = 10;
+  const passwordHash = await bcrypt.hash(req.body.password, saltRounds);
 
   //For testing
   const currentUser = req.body.username;
@@ -100,22 +101,22 @@ app.post('/signup', async (req, res) => {
   console.log({
     username: req.body.username,
     password: req.body.password,
-    passHash: passwordHash
+    passHash: passwordHash,
   });
 
-  const collection = client.db("chatapp").collection("user");
-  const  user = {username: req.body.username, password: passwordHash};
+  const collection = client.db('chatapp').collection('user');
+  const user = { username: req.body.username, password: passwordHash };
 
   // perform actions on the collection object
   collection.insertOne(user);
   res.redirect('/');
-  console.log("1 document inserted");
+  console.log('1 document inserted');
 
-  const users = collection.find({username: currentUser}).toArray()
-  .then(results => {
-    console.log(results)
-  })
-  .catch(error => console.error(error));
-
+  const users = collection
+    .find({ username: currentUser })
+    .toArray()
+    .then((results) => {
+      console.log(results);
+    })
+    .catch((error) => console.error(error));
 });
-
