8d3df8e0c87fec8674222eafba55414abc6ce5f4
Joni Laukkanen <joni.laukkanen@metropolia.fi>
2020-12-01 14:53:37 +0200

    Signup hide pass button

--- frontend/src/components/SignupForm.vue
+++ frontend/src/components/SignupForm.vue
@@ -2,19 +2,22 @@
   <div>
     <h3>signup</h3>
     <form @submit.prevent="handleSignup">
-      <div>
-        <label for="username">username</label>
+      <div id="formInputs">
         <div>
-          <input v-model="username" type="text" name="username" pattern="[A-Za-z1-9]{2,}"
-          title="Must contain atleast two characters. No special characters!" required/>
+          <label for="username">username</label>
+          <div>
+            <input v-model="username" type="text" name="username" pattern="[A-Za-z1-9]{2,}"
+            title="Must contain atleast two characters. No special characters!" required/>
+          </div>
         </div>
-      </div>
-      <div>
-        <label for="password">password</label>
         <div>
-          <input v-model="password" type="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-  title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-  required/>
+          <label for="password">password</label>
+          <div id="passwordDiv">
+            <input :type="passwordFieldType" v-model="password" class="input" name="password" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                    title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                    required />
+            <button type="button" @click="toggleVisibility" id="togglePass" class="fa fa-eye" aria-hidden="true"></button>
+          </div>
         </div>
       </div>
       <button>signup</button>
@@ -31,6 +34,7 @@ export default {
     return {
       username: '',
       password: '',
+      passwordFieldType: 'password'
     };
   },
   methods: {
@@ -44,12 +48,35 @@ export default {
       userService.create(newUser);
       this.$router.push('/home');
     },
+    toggleVisibility() {
+      this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password'
+    },
   },
 };
 </script>
 
 <style scoped>
+#formInputs {
+  justify-content: center;
+}
 input:valid {
-    border: 2px solid green;
+  border: 2px solid green;
+}
+#passwordDiv {
+  position:relative;
+  margin-bottom:10px;
+  left: 13px;
+}
+#togglePass {
+  position:relative;
+  right: 25px;
+  border:none;
+  border-radius:100%;
+  outline:none;
+  text-align:center;
+  padding:2px;
+  font-size: 0.8em;
+  color: black;
+  background: none;
 }
 </style>
